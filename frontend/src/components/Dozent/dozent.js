a57aeb5e2b803d159cf32bc1f496c939c047bbe0
nibu242 <nikita.bulokhov@aconso.com>
2023-06-20 14:58:51 +0200

    submit lecturer to api

--- frontend/src/components/Dozent/dozent.js
+++ frontend/src/components/Dozent/dozent.js
@@ -1,18 +1,50 @@
 import React from "react";
-import {Box, Input, Text} from 'dracula-ui'
+import { Box, TextField, Button } from '@mui/material';
 
 const Dozent = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    // Extract the form data
+    const data = new FormData(event.target);
+  
+    // Convert the form data to an object
+    const value = Object.fromEntries(data.entries());
+  
+    // Ensure that the keys match the expected format of the backend
+    const payload = {
+      firstName: value.firstName,
+      lastName: value.lastName,
+      email: value.email,
+    };
+  
+    // Send the form data to your backend server
+    fetch('http://localhost:9090/lecturers', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      if (response.ok) {
+        // Handle successful submission
+        console.log('Data submitted successfully');
+      } else {
+        // Handle errors
+        console.error('Error submitting data:', response);
+      }
+    });
+  };
+  
 
-    return (
-        <Box color ="" width="md">
-            <Text color="black" weight="bold">Vorname:</Text>
-            <Input placeholder="Input" />
-            <Text color="black" weight="bold">Nachname:</Text>
-            <Input placeholder="Input" />
-            <Text color="black" weight="bold">E-Mail:</Text>
-            <Input placeholder="Input" type="email" />
-        </Box>
+  return (
+    <Box component="form" onSubmit={handleSubmit} sx={{ width: 'md', p: 2 }}>
+      <TextField label="Vorname" name="firstName" fullWidth />
+      <TextField label="Nachname" name="lastName" fullWidth />
+      <TextField label="E-Mail" name="email" type="email" fullWidth />
+      <Button type="submit" variant="contained" color="primary">Submit</Button>
+    </Box>
+  );
+};
 
-    )
-}
 export default Dozent;
