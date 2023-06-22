dfcc2b85836b04bb257b18f62ee7eeb0aa5ff174
nibu242 <nikita.bulokhov@aconso.com>
2023-06-22 21:17:53 +0200

    crud studyprogram

--- frontend/src/components/ItemCard/StudyProgramCard.js
+++ frontend/src/components/ItemCard/StudyProgramCard.js
@@ -4,31 +4,29 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
 
-const StudyProgramCard = ({studyProgram, handleOpen}) => {
-    return(
-        <Card>
-            <CardContent>
-                <Box>
-                    <Typography variant="h5">{studyProgram.shortName}</Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>{studyProgram.name}</Typography>
-                </Box>
-                <Typography>Vorlesungen</Typography>
-                <Box>
-
-                </Box>
-                <Typography>Dozenten</Typography>
-                <Typography>Klassen</Typography>
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 'auto' }}>
-                
-                <IconButton aria-label="add to favorites">
-          <DeleteIcon />
+const StudyProgramCard = ({ studyProgram, handleOpen, handleDelete }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Box>
+          <Typography variant="h5">{studyProgram.shortName}</Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>{studyProgram.name}</Typography>
+        </Box>
+        <Typography>Vorlesungen</Typography>
+        <Box>
+        </Box>
+        <Typography>Dozenten</Typography>
+        <Typography>Klassen</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 'auto' }}>
+        <IconButton aria-label="delete" onClick={() => handleDelete(studyProgram.id)}>
+            <DeleteIcon />
         </IconButton>
-        <IconButton aria-label="share" onClick={() => handleOpen(studyProgram)}>
-          <EditIcon />
-        </IconButton>
-                </Box>
-            </CardContent>
-        </Card>
-    );
+          <IconButton aria-label="edit" onClick={() => handleOpen(studyProgram)}>
+            <EditIcon />
+          </IconButton>
+        </Box>
+      </CardContent>
+    </Card>
+  );
 }
-export default  StudyProgramCard;
+export default StudyProgramCard;
