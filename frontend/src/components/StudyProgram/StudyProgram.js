095f808faa06878a1e9b527b90ebc4135f0b5081
nibu242 <nikita.bulokhov@aconso.com>
2023-06-23 09:47:01 +0200

    snackbar studyprogram

--- frontend/src/components/StudyProgram/StudyProgram.js
+++ frontend/src/components/StudyProgram/StudyProgram.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import StudyProgramCard from '../ItemCard/StudyProgramCard';
-import { Autocomplete, Grid, TextField, Typography } from '@mui/material';
+import { TextField, Grid, Typography, Autocomplete, Snackbar, Alert } from '@mui/material';
 import FormModal from '../FormModal/FormModal';
 import ContentContainer from '../ContentContainer/ContentContainer';
 
@@ -21,6 +21,26 @@ const StudyProgram = () => {
   const [shortName, setShortName] = useState('');
   const [title, setTitle] = useState('');
   const [refreshKey, setRefreshKey] = useState(0);
+  const [snackPack, setSnackPack] = React.useState([]);
+  const [openSnack, setOpenSnack] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState(undefined);
+  const [snackbarMessage, setSnackbarMessage] = React.useState('');
+
+  React.useEffect(() => {
+    if (snackPack.length && !snackbarMessage) {
+      setSnackbarMessage(snackPack[0].message);
+      setMessageInfo({ ...snackPack[0] });
+      setOpenSnack(true);
+      setSnackPack((prev) => prev.slice(1));
+    } else if (snackPack.length > 0 && messageInfo && openSnack === false) {
+      setSnackbarMessage('');
+      setMessageInfo(undefined);
+    }
+  }, [snackPack, messageInfo, openSnack, snackbarMessage]);
+
+  useEffect(() => {
+    fetchData('http://localhost:9090/studyprograms', setStudyPrograms);
+  }, [refreshKey]);
 
   const fetchData = async (url, setData) => {
     try {
@@ -37,11 +57,6 @@ const StudyProgram = () => {
     }
   };
 
-  useEffect(() => {
-    fetchData('http://localhost:9090/studyprograms', setStudyPrograms);
-}, [refreshKey]);
-
-
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -52,18 +67,18 @@ const StudyProgram = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  
+
     // Start creating the payload with the form data
     const payload = {
       name: name,
       shortName: shortName,
-      id: programId, // <-- Include ID here
+      id: programId,
     };
-  
+
     // Determine the appropriate method and URL for the operation
     const method = (title === "Studienprogramm ändern") ? 'PUT' : 'POST';
     const url = 'http://localhost:9090/studyprograms/';
-  
+
     // Send the form data to our backend server
     fetch(url, {
       method: method,
@@ -75,13 +90,15 @@ const StudyProgram = () => {
       if (response.ok) {
         console.log('Data submitted successfully');
         setRefreshKey(oldKey => oldKey + 1); // trigger a refetch
+        setSnackPack((prev) => [...prev, { message: (title === "Studienprogramm ändern") ? 'Daten erfolgreich geändert!' : 'Studienprogramm wurde angelegt!', severity: 'success' }]);
       } else {
         console.error('Error submitting data:', response);
+        setSnackPack((prev) => [...prev, { message: 'Fehler beim Verarbeiten der Daten!', severity: 'error' }]);
       }
     });
   };
-  
-  
+
+
   const handleDelete = (id) => {
     // Optimistically remove the study program from the local state
     setStudyPrograms(prevPrograms => prevPrograms.filter(program => program.id !== id));
@@ -89,37 +106,51 @@ const StudyProgram = () => {
     fetch(`http://localhost:9090/studyprograms/${id}`, {
       method: 'DELETE',
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.log('Error deleting the study program: ', error);
-        // If the delete operation failed, refetch the data
+    .then(response => {
+      if (response.ok) {
+        setSnackPack((prev) => [...prev, { message: 'Studienprogramm wurde entfernt!', severity: 'success' }]);
+      } else {
+        setSnackPack((prev) => [...prev, { message: 'Fehler beim Entfernen des Studienprogramms!', severity: 'error' }]);
         setRefreshKey(oldKey => oldKey + 1);
-      });
-  };
+      }
+    });
+}
 
   const handleOpen = (studyProgram) => {
     if (studyProgram != null) {
       setName(studyProgram.name);
       setShortName(studyProgram.shortName);
-      setProgramId(studyProgram.id); // <-- Store ID here
+      setProgramId(studyProgram.id);
+      //   setLectures(studyProgram.lectures);
+      //  selectedLecturers(studyProgram.lecturers);
+      //  setStudyClasses(studyProgram.studyClasses);
       setTitle("Studienprogramm ändern")
     } else {
       setName('');
       setShortName('');
       setProgramId(null); // No program selected when adding a new one
+      //  setLectures(studyProgram.lectures);
+      //  selectedLecturers(studyProgram.lecturers);
+      //  setStudyClasses(studyProgram.studyClasses);
       setTitle("Studienprogramm hinzufügen")
     }
     setOpen(true)
-}
+  }
 
+  const processQueue = () => {
+    if (snackPack.length > 0) {
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpenSnack(true);
+    }
+  };
+
+  const handleSnackClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnack(false);
+  };
 
   const handleStudyClassesSelection = (event, value) => {
     setSelectedStudyClasses(value);
@@ -134,73 +165,98 @@ const StudyProgram = () => {
   };
 
   return (
-    <ContentContainer handleOpen={handleOpen}>
-      <FormModal open={open} handleClose={handleClose} handleSubmit={handleSubmit}>
-        <Typography>{title}</Typography>
-        <TextField
-          id="name"
-          label="Name"
-          name="name"
-          margin="normal"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          fullWidth
-        />
-        <TextField
-          id="shortName"
-          label="Kürzel"
-          name="shortName"
-          margin="normal"
-          value={shortName}
-          onChange={(event) => setShortName(event.target.value)}
-          fullWidth
-        />
-        <Autocomplete
-          multiple
-          options={studyClasses}
-          getOptionLabel={(studyClass) => studyClass.name}
-          value={selectedStudyClasses}
-          onChange={handleStudyClassesSelection}
-          renderInput={(params) => (
-            <TextField {...params} label="Studienklassen" name="studyClasses" margin="normal" />
-          )}
-        />
-        <Autocomplete
-          multiple
-          options={lectures}
-          getOptionLabel={(lecture) => lecture.lectureName}
-          value={selectedLectures}
-          onChange={handleLecturesSelection}
-          renderInput={(params) => (
-            <TextField {...params} label="Lehrveranstaltungen" name="lectures" margin="normal" />
-          )}
-        />
-        <Autocomplete
-          multiple
-          options={lecturers}
-          getOptionLabel={(lecturer) => lecturer.firstName + " " + lecturer.lastName}
-          value={selectedLecturers}
-          onChange={handleLecturersSelection}
-          renderInput={(params) => (
-            <TextField {...params} label="Dozent" name="lectureres" margin="normal" />
-          )}
-        />
-      </FormModal>
-      {studyPrograms && (
-        <Grid container spacing={2}>
-          {studyPrograms.map(item => (
-            <Grid item xs={4}>
-              <StudyProgramCard
-                key={item.id}
-                studyProgram={item}
-                handleOpen={handleOpen}
-                handleDelete={handleDelete} // Pass handleDelete to the Card
-              />
-            </Grid>
-          ))}
-        </Grid>
-      )}
-    </ContentContainer>
+    <>
+      <ContentContainer handleOpen={handleOpen}>
+        <FormModal open={open} handleClose={handleClose} handleSubmit={handleSubmit}>
+          <Typography>{title}</Typography>
+          <TextField
+            id="name"
+            label="Name"
+            name="name"
+            margin="normal"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            id="shortName"
+            label="Kürzel"
+            name="shortName"
+            margin="normal"
+            value={shortName}
+            onChange={(event) => setShortName(event.target.value)}
+            fullWidth
+          />
+          <Autocomplete
+            multiple
+            options={studyClasses}
+            getOptionLabel={(studyClass) => studyClass.name}
+            value={selectedStudyClasses}
+            onChange={handleStudyClassesSelection}
+            renderInput={(params) => (
+              <TextField {...params} label="Studienklassen" name="studyClasses" margin="normal" />
+            )}
+          />
+          <Autocomplete
+            multiple
+            options={lectures}
+            getOptionLabel={(lecture) => lecture.lectureName}
+            value={selectedLectures}
+            onChange={handleLecturesSelection}
+            renderInput={(params) => (
+              <TextField {...params} label="Lehrveranstaltungen" name="lectures" margin="normal" />
+            )}
+          />
+          <Autocomplete
+            multiple
+            options={lecturers}
+            getOptionLabel={(lecturer) => lecturer.firstName + " " + lecturer.lastName}
+            value={selectedLecturers}
+            onChange={handleLecturersSelection}
+            renderInput={(params) => (
+              <TextField {...params} label="Dozent" name="lectureres" margin="normal" />
+            )}
+          />
+        </FormModal>
+        {studyPrograms && (
+          <Grid container spacing={2}>
+            {studyPrograms.map(item => (
+              <Grid item xs={4}>
+                <StudyProgramCard
+                  key={item.id}
+                  studyProgram={item}
+                  handleOpen={handleOpen}
+                  handleDelete={handleDelete} // Pass handleDelete to the Card
+                />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </ContentContainer>
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={openSnack}
+        autoHideDuration={4000}
+        onClose={handleSnackClose}
+        onExited={() => {
+          processQueue();
+          setSnackbarMessage('');
+        }}
+        style={{ bottom: 50 }}
+      >
+        <Alert
+          onClose={handleSnackClose}
+          severity={messageInfo ? messageInfo.severity : "success"}
+          sx={{ width: '100%' }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </>
   )
 }
 export default StudyProgram;
